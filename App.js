1c876e9ce444cf3fe110b23a6f451723d8968655
DeAnna Nicholson <13dpn13@gmail.com>
2023-11-26 07:41:04 -0800

    fixed checkboxes and delete button

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
 import {
+    Button, 
+    SafeAreaView, 
+    StyleSheet, 
+    ScrollView,
     View,
     TextInput,
-    Button,
     FlatList,
     TouchableOpacity,
     Text,
 } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from "expo-status-bar";
+import Checkbox from 'expo-checkbox';
+
 
 
 
@@ -15,6 +21,12 @@ export default function App() {
     const [shoppingList, setShoppingList] = useState([]);
     const [item, setItem] = useState("");
     const [notes, setNotes] = useState("");
+    const [checked, setChecked] = useState(false);
+    const [selected, setSelected] = useState([]);
+
+
+
+
 
     useEffect(() => {
         // Load shopping list from storage when the app starts
@@ -55,12 +67,20 @@ export default function App() {
         ]);
         setItem("");
         setNotes("");
+        setChecked(false);
     };
 
-    const handleDelete = (id) => {
-        const newList = shoppingList.filter((item) => item.id !== id);
+    // const handleDelete = (id) => {
+    //     const newList = shoppingList.filter((item) => item.id !== id);
+    //     setShoppingList(newList);
+    // };
+
+    const handleDelete = () => {
+        const newList = shoppingList.filter((item) => !item.checked);
         setShoppingList(newList);
     };
+
+
 
     const handleCheck = (id) => {
         const newList = shoppingList.map((item) =>
@@ -75,50 +95,75 @@ export default function App() {
                 flexDirection: "row",
                 alignItems: "center",
                 justifyContent: "space-between",
-                padding: 10,
+                padding: 5,
+                alignContent: 'space-between',
+                alignItems: 'flex-start'
             }}
             onPress={() => handleCheck(item.id)}
         >
-            <View
+            <View  
                 style={{
-                    flexDirection: "column",
-                    marginRight: 25,
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'flex-start'
                 }}
             >
-                <Text
-                    style={{
-                        textDecorationLine: item.checked
-                            ? "line-through"
-                            : "none",
-                        color: item.checked ? "gray" : "blue",
-                        fontSize: 18,
-                        fontStyle: "italic",
-                        fontWeight: "bold",
-                        marginTop: 1,
-                        marginLeft: 5,
+                <Checkbox
+                    value={item.checked}
+                    onValueChange={() => {
+                        setChecked(!checked);
+                        setSelected(selected);
                     }}
+                    style={styles.checkbox}
                 >
-                    {item.item}
-                </Text>
-                <Text
-                    style={{
-                        textDecorationLine: item.checked
-                            ? "line-through"
-                            : "none",
-                        color: item.checked ? "gray" : "purple",
-                        fontSize: 14,
-                        marginTop: 1,
-                        marginLeft: 15,
-                    }}
-                >
-                    {item.notes}
-                </Text>
-            </View>
-            <View style={{ flexDirection: "row" }}>
+                    {item.checked}
+                </Checkbox>
+                    <View
+                        style={{
+                            flexDirection: "column",
+                            marginRight: 25,
+                            marginLeft: 5,
+                            justifyContent: "space-between",
+                        }}
+                    >
+
+                        <Text
+                            style={{
+                                textDecorationLine: item.checked
+                                    ? "line-through"
+                                    : "none",
+                                color: item.checked ? "gray" : "blue",
+                                fontSize: 18,
+                                fontStyle: "italic",
+                                fontWeight: "bold",
+                                marginTop: 1,
+                                marginLeft: 5,
+                                textAlign: "auto"
+                            }}
+                        >
+                            {item.item}
+                        </Text>
+                        <Text
+                            style={{
+                                textDecorationLine: item.checked
+                                    ? "line-through"
+                                    : "none",
+                                color: item.checked ? "gray" : "purple",
+                                fontSize: 14,
+                                marginTop: 1,
+                                marginLeft: 15,
+                                textAlign: "auto"
+                            }}
+                        >
+                            {item.notes}
+                        </Text>
+                    </View>
+                </View>
+            {/* <View style={{ flexDirection: "row" }}>
                 <TouchableOpacity onPress={() => handleDelete(item.id)}>
                     <Text style={{ color: "red" }}>Delete</Text>
                 </TouchableOpacity>
-            </View>
+            </View> */}
         </TouchableOpacity>
     );
 
@@ -169,6 +214,53 @@ export default function App() {
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id.toString()}
             />
+            <View style={styles.delete} >
+                <Button
+                    title="Delete Checked Item"
+                    onPress={() => handleDelete()}
+                    color='red'
+                />
+            </View>
+            <StatusBar style="auto" />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        marginTop: 35,
+        marginLeft: 15,
+        alignContent: 'center',
+        marginRight: 15,
+    },
+    input: {
+        height: 40,
+        width: '50%',
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    form: {
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    list: {
+        marginTop: 20,
+    },
+    title: {
+        justifyContent: 'center',
+        fontSize: 24,
+        fontStyle: 'italic',
+        color: 'purple',
+        maxWidth: '50%',
+        marginLeft: 20,
+        marginTop: 20,
+    },
+    delete: {
+        marginTop: 20,
+        alignContent: 'bottom'
+    },
+});
+
